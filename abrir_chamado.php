07ea73b6e31dff7c12221fdd592782f11c0415dc
Ryan-lima13 <79115469+Ryan-lima13@users.noreply.github.com>
2021-05-21 00:21:04 -0300

    Update abrir_chamado.php

--- abrir_chamado.php
+++ abrir_chamado.php
@@ -2,6 +2,8 @@
 <html lang="pt-br">
   <head>
     <title>Abrir Chamado</title>
+
+    <link rel="stylesheet" href="css/style.css">
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -10,6 +12,70 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
+      <header> <!-- inicio cabeçalho -->
+
+        <nav class="navbar navbar-expand-md navbar-light bg-dark">
+            <a href="index.php">
+                <img class="navbar-brand" width="50" src="imagem/logo.png" alt="">
+                Help Desk
+            </a>
+
+        </nav>
+
+    </header> <!-- fim cabeçalho -->
+
+    
+  <div class="container">    
+      <div class="row">
+
+        <div class="card-abrir-chamado">
+          <div class="card">
+            <div class="card-header">
+              Abertura de chamado
+            </div>
+            <div class="card-body">
+              <div class="row">
+                <div class="col">
+                  
+                  <form>
+                    <div class="form-group">
+                      <label>Título</label>
+                      <input type="text" class="form-control" placeholder="Título">
+                    </div>
+                    
+                    <div class="form-group">
+                      <label>Categoria</label>
+                      <select class="form-control">
+                        <option>Criação Usuário</option>
+                        <option>Impressora</option>
+                        <option>Hardware</option>
+                        <option>Software</option>
+                        <option>Rede</option>
+                      </select>
+                    </div>
+                    
+                    <div class="form-group">
+                      <label>Descrição</label>
+                      <textarea class="form-control" rows="3"></textarea>
+                    </div>
+
+                    <div class="row mt-5">
+                      <div class="col-6">
+                        <button class="btn btn-lg btn-warning btn-block" type="submit">Voltar</button>
+                      </div>
+
+                      <div class="col-6">
+                        <button class="btn btn-lg btn-info btn-block" type="submit">Abrir</button>
+                      </div>
+                    </div>
+                  </form>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
       
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
