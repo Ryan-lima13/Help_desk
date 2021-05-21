525dbb2ffd9dfd53a5b5c05ace13341a0544f7e1
Ryan-lima13 <79115469+Ryan-lima13@users.noreply.github.com>
2021-05-21 00:11:32 -0300

    Update home.php

--- home.php
+++ home.php
@@ -23,6 +23,28 @@
         </nav>
 
     </header> <!-- fim cabeçalho -->
+
+    <div class="container">    
+      <div class="row">
+
+        <div class="card-home">
+          <div class="card">
+            <div class="card-header">
+              Menu
+            </div>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-6 d-flex justify-content-center">
+                  <img src="imagem/formulario_abrir_chamado.png" width="70" height="70">
+                </div>
+                <div class="col-6 d-flex justify-content-center">
+                  <img src="imagem/formulario_consultar_chamado.png" width="70" height="70">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
       
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
