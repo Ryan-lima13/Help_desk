0b5e83d24e0483e3844ce9e9ec11f54bcc85b935
Ryan-lima13 <79115469+Ryan-lima13@users.noreply.github.com>
2021-05-22 21:16:19 -0300

    Update validacao_login.php

--- validacao_login.php
+++ validacao_login.php
@@ -1,5 +1,6 @@
 <?php
-
+    // variavel de autenticação 
+    $usuario_autenticado = false;
     // usuário do sistemas
 
     $usuarios = array(
@@ -18,6 +19,17 @@
     echo '<br/>';
     echo $_GET['senha'];
     */
+
+    foreach($usuarios as $user){
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
+            $usuario_autenticado = true;
+        }
+        if($usuario_autenticado){
+            echo 'login e senha autenticados';
+        }else{
+            echo 'erro nao autenticado';
+        }
+    }
     print_r($_POST);
     echo '<br/>';
     echo $_POST['email'];
