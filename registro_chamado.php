fcd5ae1298cc070bcd4e01fd647a302e4e70402d
Ryan-lima13 <79115469+Ryan-lima13@users.noreply.github.com>
2021-05-28 19:45:55 -0300

    Update registro_chamado.php

--- registro_chamado.php
+++ registro_chamado.php
@@ -1,18 +1,4 @@
 <?php
 
-    echo '<pre>';
-    print_r($_POST);
-    echo '</pre>';
-
-    $arquivo = fopen('arquivo.rd','a');
-    $titulo = str_replace('#',',',$_POST['titulo']);
-    $categoria = str_replace('#',',',$_POST['categoria']);
-    $descricao = str_replace('#',',',$_POST['descricao']);
-    $texto = $_POST['titulo'] .'#'.$_POST['categoria'] .'#'.$_POST['descricao'].PHP_EOL;
-
-    fwrite($arquivo,$texto);
-    fclose($arquivo);
-
-    header('location:abrir_chamado.php');
-    //echo " $texto <br/> ";
+   
 ?>
