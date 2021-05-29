f2f310d7fcd00dc1e24cb8ee3e850340e2e2de7a
Ryan-lima13 <79115469+Ryan-lima13@users.noreply.github.com>
2021-05-29 17:33:26 -0300

    Update registro_chamado.php

--- registro_chamado.php
+++ registro_chamado.php
@@ -1,6 +1,10 @@
 <?php
     echo '<pre>'; 
     print_r ($_POST);
-    echo '</pre>'
+    echo '</pre>';
+
+    //fopen('arquivo.hd','a',);
+    $texto = $_POST['titulo'].$_POST['categoria'].$_POST['descricao'];
+    echo $texto;
    
 ?>
