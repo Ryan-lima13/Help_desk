0afc238b48d44d4c64a7e82e4d060e5f25236d99
Ryan-lima13 <79115469+Ryan-lima13@users.noreply.github.com>
2021-05-29 17:39:23 -0300

    Update registro_chamado.php

--- registro_chamado.php
+++ registro_chamado.php
@@ -7,7 +7,7 @@
     $titulo= str_replace('#','-',$_POST['titulo']);
     $categoria = str_replace('#','-',$_POST['categoria']);
     $descricao = str_replace('#','-',$_POST['descricao']);
-    $texto = $_POST['titulo'].'#'.$_POST['categoria'].'#'.$_POST['descricao'];
+    $texto = $titulo.'#'.$categoria.'#'.$descricao;
     echo $texto;
    
 ?>
